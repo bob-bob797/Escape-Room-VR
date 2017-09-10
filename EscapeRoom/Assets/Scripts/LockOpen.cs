796950d0ddd8cb16c0c37bebf2a81fd02324c2ee
footballmoose797@yahoo.com <footballmoose797@yahoo.com>
2017-09-09 23:37:21 -0400

    Created openable chest and API for openable objects with key

---
+++ EscapeRoom/Assets/Scripts/LockOpen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockOpen : MonoBehaviour {
+	public bool locked;
+	public GameObject lid;
+	public GameObject key;
+
+	void OnTriggerEnter(Collider collision) {
+		Debug.Log (collision.gameObject.name);
+		if (collision.gameObject == key) {
+			lid.GetComponent<openLid> ().open ();
+		}
+	}
+}
---
+++ EscapeRoom/Assets/Scripts/openLid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openLid : MonoBehaviour {
+	public bool opened;
+	public float smooth;
+	public float rotationTime;
+	public Vector3 finalRotation;
+	public GameObject rotatePoint;
+
+	private float endTime;
+
+	public void open() {
+		opened = true;
+
+		endTime = Time.time + rotationTime;
+	}
+
+	void Update () {
+		if (opened && Time.time < endTime) {
+			transform.RotateAround (rotatePoint.transform.position, finalRotation, Time.deltaTime * smooth);
+		}
+	}
+}
